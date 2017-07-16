559df61de9d43708dda6e013c45d1bee3a92f18e
Sasha Morgan <sashamorgan2015@gmail.com>
2017-07-16 16:38:16 -0400

    Simple Hello World Program

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hello World
+By Sasha Morgan
+
+##Project Description
+Everyone's favorie first program, "Hello World"
---
+++ homework1-SashaMorgan.cpp
@@ -0,0 +1,36 @@
+/**
+* @file <homework1-SashaMorgan.cpp>
+* <The purpose of this program is to say Hello World to whoever opens the program.>
+* @date   <8/31/15>
+* @author <Sasha Morgan and sasha97> 
+
+* Virginia Tech Honor Code Pledge
+* On my honor:
+*
+* - I have not discussed the C++ language code in my program with
+* anyone other than my instructor or the teaching assistants
+* assigned to this course.
+* - I have not used C++ language code obtained from another student,
+* or any other unauthorized source, either modified or unmodified.
+* - If any C++ language code or documentation used in my program
+* was obtained from another source, such as a text book of coarse
+* notes, that has been clearly noted with a proper citation in
+* the comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the Web-Cat Server.
+* <Sasha Morgan>
+*/
+
+
+#include <iostream>
+using namespace std;
+/**
+* <This is the start of the main function, the main function will create a window that displays the text "Hello World!">
+*
+*@return 0
+*/
+int main()
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
